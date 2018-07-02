6a237dee9ef1937f44ed8dd0ba2fec82dd1b7f2b
Philip Damra <philip.damra@gstv.com>
2018-07-02 14:10:24 -0700

    Hello next.js

---
+++ hello-next/.next/4.fb4cdbc96bef85449b99.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate(4,{
+
+/***/ "./node_modules/webpack/buildin/harmony-module.js":
+false,
+
+/***/ "./pages/index.js":
+/***/ (function(module, __webpack_exports__) {
+
+"use strict";
+throw new Error("Module build failed: SyntaxError: /Users/philip.damra/Sites/_etc/learn-nextjs/hello-next/pages/index.js: Unterminated JSX contents (4:8)\n\n  2 |   <div>\n  3 |     <p>Hello Next.js\n> 4 |   </div>\n    |         ^\n  5 | );\n  6 | \n  7 | export default Index;\n    at _class.raise (/Users/philip.damra/Sites/_etc/learn-nextjs/hello-next/node_modules/babylon/lib/index.js:778:15)\n    at _class.jsxReadToken (/Users/philip.damra/Sites/_etc/learn-nextjs/hello-next/node_modules/babylon/lib/index.js:7727:16)\n    at _class.readToken (/Users/philip.damra/Sites/_etc/learn-nextjs/hello-next/node_modules/babylon/lib/index.js:8095:21)\n    at _class.nextToken (/Users/philip.damra/Sites/_etc/learn-nextjs/hello-next/node_modules/babylon/lib/index.js:976:12)\n    at _class.next (/Users/philip.damra/Sites/_etc/learn-nextjs/hello-next/node_modules/babylon/lib/index.js:912:10)\n    at _class.eat (/Users/philip.damra/Sites/_etc/learn-nextjs/hello-next/node_modules/babylon/lib/index.js:917:12)\n    at _class.expect (/Users/philip.damra/Sites/_etc/learn-nextjs/hello-next/node_modules/babylon/lib/index.js:2051:10)\n    at _class.jsxParseClosingElementAt (/Users/philip.damra/Sites/_etc/learn-nextjs/hello-next/node_modules/babylon/lib/index.js:8001:12)\n    at _class.jsxParseElementAt (/Users/philip.damra/Sites/_etc/learn-nextjs/hello-next/node_modules/babylon/lib/index.js:8020:39)\n    at _class.jsxParseElementAt (/Users/philip.damra/Sites/_etc/learn-nextjs/hello-next/node_modules/babylon/lib/index.js:8024:34)\n    at _class.jsxParseElement (/Users/philip.damra/Sites/_etc/learn-nextjs/hello-next/node_modules/babylon/lib/index.js:8077:19)\n    at _class.parseExprAtom (/Users/philip.damra/Sites/_etc/learn-nextjs/hello-next/node_modules/babylon/lib/index.js:8084:21)\n    at _class.parseExprSubscripts (/Users/philip.damra/Sites/_etc/learn-nextjs/hello-next/node_modules/babylon/lib/index.js:2725:21)\n    at _class.parseMaybeUnary (/Users/philip.damra/Sites/_etc/learn-nextjs/hello-next/node_modules/babylon/lib/index.js:2704:21)\n    at _class.parseExprOps (/Users/philip.damra/Sites/_etc/learn-nextjs/hello-next/node_modules/babylon/lib/index.js:2616:21)\n    at _class.parseMaybeConditional (/Users/philip.damra/Sites/_etc/learn-nextjs/hello-next/node_modules/babylon/lib/index.js:2588:21)");
+
+/***/ })
+
+})
+//# sourceMappingURL=4.fb4cdbc96bef85449b99.hot-update.js.map
